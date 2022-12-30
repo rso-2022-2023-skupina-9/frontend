35afff80af95ea8426337eff3b115e295a9ded70
Zan Kogovsek <zk0821@student.uni-lj.si>
2022-12-30 02:57:56 +0100

    Change Production IP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  mikrostoritevZaIzdelkeUrl: 'http://34.141.157.180:8080/v1',
-  mikrostoritevZaKosariceUrl: 'http://34.141.157.180:8081/v1'
+  mikrostoritevZaIzdelkeUrl: 'http://34.82.65.219/mikrostoritev-za-izdelke/v1',
+  mikrostoritevZaKosariceUrl: 'http://34.82.65.219/mikrostoritev-za-kosarice/v1'
 };
